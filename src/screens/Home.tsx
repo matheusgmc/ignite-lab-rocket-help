dc111db1b71cb7f4fd84ff8715074c671ee96af6
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-18 20:14:36 -0400

    Tela inicial da aplicação

---
+++ src/screens/Home.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import {
+  Center,
+  FlatList,
+  Heading,
+  HStack,
+  IconButton,
+  Text,
+  useTheme,
+  VStack,
+} from "native-base";
+import { ChatTeardropText, SignOut } from "phosphor-react-native";
+import Logo from "../assets/logo_secondary.svg";
+import { Filter } from "../components/Filter";
+import { Order, OrderProps } from "../components/Order";
+import { Button } from "../components/Button";
+import { useNavigation } from "@react-navigation/native";
+export function Home() {
+  const [statusSelected, setStatusSelected] = useState<"open" | "closed">(
+    "open"
+  );
+
+  const [orders, setOrders] = useState<OrderProps[]>([
+    {
+      id: "123",
+      patrimony: "321231",
+      when: "18/07/2022 às 10:00",
+      status: "open",
+    },
+  ]);
+
+  const navigation = useNavigation();
+
+  const { colors } = useTheme();
+
+  const handleNewOrder = () => {
+    navigation.navigate("new");
+  };
+
+  const handleOpenDetails = (orderId: string) => {
+    navigation.navigate("details", { orderId });
+  };
+
+  return (
+    <VStack flex={1} pb={6} bg={"gray.700"}>
+      <HStack
+        w={"full"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        bg={"gray.600"}
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+
+        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack
+          w={"full"}
+          mt={8}
+          mb={4}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Heading color={"gray.100"}>solicitações</Heading>
+          <Text color={"gray.200"}>{orders.length}</Text>
+        </HStack>
+        <HStack space={3} mb={8}>
+          <Filter
+            type={"open"}
+            title={"em andamento"}
+            isActive={statusSelected === "open"}
+            onPress={() => setStatusSelected("open")}
+          />
+          <Filter
+            type={"closed"}
+            title={"finalizado"}
+            isActive={statusSelected === "closed"}
+            onPress={() => setStatusSelected("closed")}
+          />
+        </HStack>
+        <FlatList
+          data={orders}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+          )}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText size={40} color={colors.gray[300]} />
+              <Text
+                color={"gray.300"}
+                fontSize={"xl"}
+                mt={6}
+                textAlign={"center"}
+              >
+                Você ainda não possui {"\n"} solicitações{" "}
+                {statusSelected === "open" ? "em andamento" : "finalizadas"}
+              </Text>
+            </Center>
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: 100,
+          }}
+        />
+        <Button title={"Nova Solicitação"} onPress={handleNewOrder} />
+      </VStack>
+    </VStack>
+  );
+}
