b6cbba6721a673f4ab5d779301311755c0fdb6dc
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-18 20:14:58 -0400

    Tela de cadastro para nova solicitação

---
+++ src/screens/Register.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { VStack, useTheme } from "native-base";
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+import { Input } from "../components/Input";
+export function Register() {
+  const { colors } = useTheme();
+  return (
+    <VStack flex={1} p={6} bg={"gray.600"}>
+      <Header title={"Nova Solicitação"} />
+
+      <Input placeholder="Número do patrimônio" mt={4} />
+
+      <Input
+        placeholder="Descrição do problema"
+        flex={1}
+        mt={5}
+        multiline
+        textAlignVertical="top"
+      />
+
+      <Button title={"Cadastrar"} mt={5} />
+    </VStack>
+  );
+}
